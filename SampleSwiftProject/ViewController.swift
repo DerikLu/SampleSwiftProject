0f7ffd4ae599828f3082c88064cd01b5fba97f92
CGO <cgo@CGOtekiMacBook-Air.local>
2014-08-09 20:15:59 +0800

    init project

---
+++ SampleSwiftProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSwiftProject
+//
+//  Created by CGO on 2014/8/9.
+//  Copyright (c) 2014年 DerikLu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
